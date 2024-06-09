26b178a44314e89c1ce84a6337be353d52f5dd10
EC2 Default User <ec2-user@ip-172-16-1-79.ec2.internal>
2024-06-09 05:34:05 +0000

    testing build again

--- helloworld.js
+++ helloworld.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
-
+//Added a comment to check for auto build to work
    // Send the response body as "Hello World"
    response.end('Hello World\n')
 }).listen(3333)
