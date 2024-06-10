53e7cf6b89fad04d23f261f472f85563305c7cda
EC2 Default User <ec2-user@ip-172-16-1-79.ec2.internal>
2024-06-10 21:22:35 +0000

    try admin

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,7 @@ var http = require("http")
 
 http.createServer(function (request, response) {
 
-   // Send the HTTP header
+   // Sedddnd the HTTP header
    // HTTP Status: 200 : OK
    // eeeContent Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
