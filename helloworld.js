2ca368194193c23390b2da9f9819db356a1f125a
EC2 Default User <ec2-user@ip-172-16-1-79.ec2.internal>
2024-06-10 20:39:10 +0000

    Adding a comment to js

--- helloworld.js
+++ helloworld.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
-//Added a comment to check for auto build to work
+//AAAdded a comment to check for auto build to work
    /// wanting to see if this works Send the response body as "Hello World"
    response.end('Hello World\n')
 }).listen(3333)
